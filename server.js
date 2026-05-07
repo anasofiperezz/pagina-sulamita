0a6d9f9b1a62a814de5b4edc4f3adca66254cdb9
anasofiperezz <anasofi.perez.sanchez@gmail.com>
2026-05-07 00:12:44 -0600

    img

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs/promises");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const dataDir = path.join(__dirname, "data");
 
